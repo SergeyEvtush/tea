2386f1b7991abc723852ff4554fc74e9010aaeb1
SergeyEvtush <evtushru208@gmail.com>
2023-12-02 00:56:51 +0200

    убрал баги при появлении меню+добавил затенение при появлении меню

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
 const burgerBtn = document.querySelector(".burger");
 const mobileMenu = document.querySelector(".menu__list");
-const mobileNav = document.querySelector(".mobile-nav");
 const closeBtn = document.querySelector(".menu__close");
+const shadowBackground = document.querySelector(".background-shadow");
 
-
-const addClass = (element,addingClass) => {
-	element.classList.toggle(addingClass);
- }
-const removeClass = (element,removingClass) => {
-	element.classList.remove(removingClass);
-}
+const addClass = (element, addingClass) => {
+  element.classList.toggle(addingClass);
+};
+const removeClass = (element, removingClass) => {
+  element.classList.remove(removingClass);
+};
 function closeMobileMenu() {
-	removeClass(mobileMenu, 'active');
-		removeClass(mobileNav, 'active');
-		removeClass(closeBtn, 'active');
- }
+  removeClass(mobileMenu, "active");
+  removeClass(shadowBackground, "active");
+}
 
 burgerBtn.addEventListener("click", (e) => {
-	e.preventDefault;
-	addClass(mobileMenu, "active");
-	addClass(mobileNav, "active");
-	addClass(closeBtn, "active");
+  e.preventDefault;
+  addClass(mobileMenu, "active");
+  addClass(shadowBackground, "active");
 });
-closeBtn.addEventListener("click", closeMobileMenu)
+
+closeBtn.addEventListener("click", closeMobileMenu);
+
 window.addEventListener("resize", () => {
-	if (window.innerWidth > 860||!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
-		closeMobileMenu();
+  if (
+    window.innerWidth > 860 ||
+    !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    closeMobileMenu();
   }
 });
