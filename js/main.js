972f167615859eaafeb20a2cc842cc1f0b22ff08
SergeyEvtush <evtushru208@gmail.com>
2023-12-01 10:38:12 +0200

    сверстал кнопку закрытия меню на мобилке и прописал логику закрытия меню на мобилке

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 const burgerBtn = document.querySelector(".burger");
 const mobileMenu = document.querySelector(".menu__list");
 const mobileNav = document.querySelector(".mobile-nav");
+const closeBtn = document.querySelector(".menu__close");
+
+
+const addClass = (element,addingClass) => {
+	element.classList.toggle(addingClass);
+ }
+const removeClass = (element,removingClass) => {
+	element.classList.remove(removingClass);
+}
+function closeMobileMenu() {
+	removeClass(mobileMenu, 'active');
+		removeClass(mobileNav, 'active');
+		removeClass(closeBtn, 'active');
+ }
 
 burgerBtn.addEventListener("click", (e) => {
-  e.preventDefault;
-  mobileMenu.classList.toggle("active");
-  mobileNav.classList.toggle("active");
+	e.preventDefault;
+	addClass(mobileMenu, "active");
+	addClass(mobileNav, "active");
+	addClass(closeBtn, "active");
 });
-window.addEventListener("resize", (e) => {
-  if (window.innerWidth > 860) {
-    mobileMenu.classList.remove("active");
-    mobileNav.classList.remove("active");
+closeBtn.addEventListener("click", closeMobileMenu)
+window.addEventListener("resize", () => {
+	if (window.innerWidth > 860||!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+		closeMobileMenu();
   }
 });
