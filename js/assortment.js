2af5f178396fbfdf91d9d29f064f60d1b56b5bfa
SergeyEvtush <evtushru208@gmail.com>
2023-12-19 00:38:09 +0200

    deleted:    js/assortment.js

--- js/assortment.js
+++
@@ -1,6 +0,0 @@
-const assortmentList = document.querySelector(".assortment__list");
-
-const response = fetch("../json-files/collection.json").then((res) =>
-  res.json()
-);
-console.log(response);
